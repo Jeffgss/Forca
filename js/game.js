e77e19cb0345d70953c5d07042f1917cb4f350e4
Jeff Santos <jefferson.santos@maxmilhas.com.br>
2021-06-29 11:57:07 -0300

    refactor game.js

--- js/game.js
+++ js/game.js
@@ -1,13 +1,13 @@
-const createGame = function (sprite) {
+const createGame = (sprite) => {
   let secretWord = "";
   let gaps = [];
   let stage = 1;
 
-  let processKick = function (kick) {
+  const processKick = (kick) => {
     if (!kick.trim()) throw new Error("Chute inválido!");
-    let exp = new RegExp(kick, "gi");
-    let result;
-    let isCorrect = false;
+    const exp = new RegExp(kick, "gi");
+    let result,
+      isCorrect = false;
 
     while ((result = exp.exec(secretWord))) {
       gaps[result.index] = kick;
@@ -17,15 +17,11 @@ const createGame = function (sprite) {
     if (!isCorrect) sprite.nextFrame();
   };
 
-  const createGaps = function () {
-    gaps = Array(secretWord.length).fill("");
-  };
+  const createGaps = () => (gaps = Array(secretWord.length).fill(""));
 
-  const nextStage = function () {
-    stage = 2;
-  };
+  const nextStage = () => (stage = 2);
 
-  const setSecretWord = function (word) {
+  const setSecretWord = (word) => {
     if (!word.trim() || word.length <= 2)
       throw new Error("Palavra secreta inválida!");
     secretWord = word;
@@ -34,45 +30,36 @@ const createGame = function (sprite) {
     nextStage();
   };
 
-  const getGaps = function () {
-    return gaps;
-  };
+  const getGaps = () => gaps;
 
-  const getStage = function () {
-    return stage;
-  };
+  const getStage = () => stage;
 
-  const win = function () {
-    return gaps.length
+  const win = () =>
+    gaps.length
       ? !gaps.some(function (gap) {
           return gap == "";
         })
       : false;
-  };
 
-  const lose = function () {
-    return sprite.isFinished();
-  };
+  const lose = () => sprite.isFinished();
 
-  const restart = function () {
+  const restart = () => {
     secretWord = "";
     gaps = [];
     stage = 1;
     sprite.reset();
   };
 
-  const winOrLose = function () {
-    return win() || lose();
-  };
+  const winOrLose = () => win() || lose();
 
   return {
-    setSecretWord: setSecretWord,
-    getGaps: getGaps,
-    getStage: getStage,
-    processKick: processKick,
-    win: win,
-    lose: lose,
-    restart: restart,
-    winOrLose: winOrLose,
+    setSecretWord,
+    getGaps,
+    getStage,
+    processKick,
+    win,
+    lose,
+    restart,
+    winOrLose,
   };
 };
